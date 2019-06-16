7b86668f3172cc15d4df88dfff1e1400f0d50bb1
Степан <stepanus97@yandex.ru>
2019-06-17 01:01:36 +0500

    Update semafor.cpp

--- semafor/semafor.cpp
+++ semafor/semafor.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>#include <ctype.h>#include <stdlib.h>#include <sys/types.h>#include <sys/ipc.h>#include <sys/sem.h>
-int main(){    key_t key = ftok("/tmp/sem.temp", 1);
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+int main(){    
+    key_t key = ftok("/tmp/sem.temp", 1);
     int semid = semget(key, 16, IPC_CREAT | 0666); // sid
 
-    struct sembuf sops[16];    for (int i = 0; i < 16; i++) {        sops[i].sem_num = i;        sops[i].sem_op = i;        sops[i].sem_flg = 0;    }    semop(semid, sops, 16);    perror("Errno after semop: ");
+    struct sembuf sops[16];    
+    for (int i = 0; i < 16; i++) {        
+        sops[i].sem_num = i;        
+        sops[i].sem_op = i;        
+        sops[i].sem_flg = 0;    
+    }    
+    semop(semid, sops, 16);    
+    perror("Errno after semop: ");
     return 0;
 }
